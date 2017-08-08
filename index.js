0401219b1c20fb6565feae801ecb51232515c055
Samuel Marks <samuelmarks@gmail.com>
2017-08-08 12:17:50 +1000

    Updated dependencies; moved to new ECMAScript syntax; added support for ES6 Promise

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
-var isFn = require('is-fn');
-var setImmediate = require('set-immediate-shim');
 
-module.exports = function (promise) {
+const isFn = require('is-fn');
+
+module.exports = promise => {
 	if (!isFn(promise.then)) {
 		throw new TypeError('Expected a promise');
 	}
 
-	return function (cb) {
-		promise.then(function (data) {
-			setImmediate(cb, null, data);
-		}, function (err) {
-			setImmediate(cb, err);
-		});
-	};
+	if (isFn(promise.catch)) {
+		return cb => promise.then(data => setImmediate(cb, null, data))
+			.catch(err => setImmediate(cb, err));
+	}
+
+	return cb => promise.then(
+		data => setImmediate(cb, null, data), err => setImmediate(cb, err)
+	);
 };
--- test.js
+++ test.js
@@ -1,30 +1,31 @@
 'use strict';
-var equal = require('assert').strictEqual;
-var promiseToCallback = require('./');
 
-['q', 'bluebird', 'pinkie-promise'].forEach(function (name) {
-	var Promise = require(name);
+const equal = require('assert').strictEqual;
+const promiseToCallback = require('./');
+
+['q', 'bluebird', 'pinkie-promise'].forEach(name => {
+	let Promise = require(name);
 	if (Promise.Promise) {
 		Promise = Promise.Promise;
 	}
 
-	it(name + ': success', function (done) {
-		var success = new Promise(function (resolve) {
-			resolve('success');
-		});
+	it(name + ': success', done => {
+		const success = new Promise(resolve =>
+			resolve('success')
+		);
 
-		promiseToCallback(success)(function (err, data) {
+		promiseToCallback(success)((err, data) => {
 			equal(data, 'success');
 			done();
 		});
 	});
 
-	it(name + ': failure', function (done) {
-		var failure = new Promise(function (resolve, reject) {
-			reject('failure');
-		});
+	it(name + ': failure', done => {
+		const failure = new Promise((resolve, reject) =>
+			reject('failure')
+		);
 
-		promiseToCallback(failure)(function (err) {
+		promiseToCallback(failure)(err => {
 			equal(err, 'failure');
 			done();
 		});
